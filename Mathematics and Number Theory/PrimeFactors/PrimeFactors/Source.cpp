00d86fc82655399dd0e2261013a8b434b855c549
Kemar Galloway <kemargalloway@gmail.com>
2016-09-15 12:42:15 -0400

    Prime Factors
    finding the prime factors of entered numbers.

---
+++ Mathematics and Number Theory/PrimeFactors/PrimeFactors/Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+void findPrimeFactors(int n)
+{
+	for (int i = n; i % 2 == 0; i = i / 2) {
+		printf("%d", 2);
+	}
+
+	for (int i = 3; i <= sqrt(n); i = i + 2) {
+		for (i = i; n % i == 0; n = n / i) {
+			printf("%d", i);
+		}
+	}
+
+	if (n > 2) {
+		printf("%d ",n);
+	}
+		
+}
+
+int main()
+{
+	int n = 0;
+	std::cout << "Enter a number: " << std::endl;
+	std::cin >> n;
+	findPrimeFactors(n);
+	return 0;
+}
